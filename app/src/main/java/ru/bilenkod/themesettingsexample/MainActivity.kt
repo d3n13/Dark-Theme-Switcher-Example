95513aa9533d5d0d77123fdd26e4a2c91e957795
d3n13 <bilenkod@mail.ru>
2019-02-06 19:07:16 +0500

    Code made prettier

--- app/src/main/java/ru/bilenkod/themesettingsexample/MainActivity.kt
+++ app/src/main/java/ru/bilenkod/themesettingsexample/MainActivity.kt
@@ -13,29 +13,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+        if (checkAndSetNightMode()) {
             setTheme(R.style.DarkTheme)
-            isNightMode = true
         }
         setContentView(R.layout.activity_main)
 
         val switch: Switch = findViewById(R.id.switch_night_mode)
         switch.isChecked = isNightMode
-
-        switch.setOnCheckedChangeListener{ _: CompoundButton, nightModeOn: Boolean ->
+        switch.setOnCheckedChangeListener { _: CompoundButton, nightModeOn: Boolean ->
             swapTheme(nightModeOn)
         }
     }
 
-    private fun swapTheme(nightModeOn: Boolean) {
-        val current: Int = AppCompatDelegate.getDefaultNightMode()
-        var currentNightModeOn = false
-
-        if (current == AppCompatDelegate.MODE_NIGHT_YES) {
-            currentNightModeOn = true
+    private fun checkAndSetNightMode(): Boolean {
+        var isNightModeCurrentlyOn = false
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            isNightModeCurrentlyOn = true
         }
+        isNightMode = isNightModeCurrentlyOn
+        return isNightModeCurrentlyOn
+    }
 
-        if (currentNightModeOn != nightModeOn) {
+    private fun swapTheme(nightModeOn: Boolean) {
+        if (isNightMode != nightModeOn) {
             when (nightModeOn) {
                 true -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
                 false -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
